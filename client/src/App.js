1bca90a928ecfea89dfcca513be22f18f6ba2692
denvy <maks.denv.terehov@gmail.com>
2021-04-08 20:59:37 +0300

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import 'materialize-css'
+import {useRoutes} from "./routes"
+import {BrowserRouter} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="container">
-     <h1>Hello</h1>
-    </div>
-  );
+    const routes = useRoutes(false)
+      return (
+        <div className="container">
+            <BrowserRouter>{routes}</BrowserRouter>
+        </div>
+      )
 }
 
 export default App
